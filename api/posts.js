2932e7a34238453c389349c737235d9adb8c67fe
Andrew <root@minimyl.localdomain>
2022-10-31 14:59:07 -0400

    Giving the correct functions

--- api/posts.js
+++ api/posts.js
@@ -1,6 +1,35 @@
 const express = require('express');
 const postsRouter = express.Router();
-const { getAllPosts } = require("../db")
+const { getAllPosts, createPostTag, createPosts } = require("../db")
+const { requireUser } = require("./utils");
+
+postsRouter.post('/', requireUser, async (req, res, next) => {
+    const { title, content, tags = "" } = req.body;
+
+    const tagArr = tags.trim().split(/\s+/)
+    const postData ={title, content, authorId: req.user.id}
+console.log(req.user, "This works")
+
+    if (tagArr.length) {
+        postData.tags = tagArr
+    }
+
+    try {
+        
+        const post = await createPosts(postData);
+
+        if(post) {
+            res.send( post )
+        } else {
+            next();
+        }
+
+    } catch ({name, message}) {
+        next({name, message})
+    }
+
+})
+
 postsRouter.use((req, res, next) => {
     console.log("A request is being made to /posts")
 

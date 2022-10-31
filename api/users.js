c3518b0dc2f1073e3482451c238a58f4d07bdd22
Nelson <n911luna@gmail.com>
2022-10-31 13:09:45 -0500

    added user sign up feature and allows no duplicates

--- api/users.js
+++ api/users.js
@@ -6,7 +6,7 @@ token;
 
 
 
-const { getAllUsers, getUserByUsername } = require("../db")
+const { getAllUsers, getUserByUsername, createUser } = require("../db")
 usersRouter.use((req,res,next) => {
     console.log("A request is being made to /users")
     
@@ -47,5 +47,41 @@ usersRouter.post('/login', async (req, res, next) => {
         next(error);
     }
 });
+
+usersRouter.post('/register',async (req,res,next) => {
+    const {username,password,name,location} = req.body
+
+    try {
+        const _user = await getUserByUsername(username)
+
+        if (_user){
+            next({
+                name: 'UserExistError',
+                message: "A user by that username already exists."
+            })
+        }
+
+        const user = await createUser({username,password,name,location})
+
+        const token = jwt.sign({
+            id: user.id,
+            username
+         },process.env.JWT_SECRET,{
+            expiresIn:'1w'
+        })
+
+        res.send({
+            message: 'Thank you for signing up!',
+            token
+        });
+
+    } catch ({name,message}) {
+        next({name,message});
+    }
+})
+
+
+
+
 
 module.exports = usersRouter;
